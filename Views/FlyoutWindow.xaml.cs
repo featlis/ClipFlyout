c96104ee5a8680f6866c84aaebb283c48ed26e13
featlis <featlis.celenear@gmail.com>
2026-09-04 17:35:45 +0900

    feat: implement FlyoutWindow UI with entry/exit animations and content previews

--- Views/FlyoutWindow.xaml.cs
+++ Views/FlyoutWindow.xaml.cs
@@ -65,6 +65,7 @@ public partial class FlyoutWindow : Window
     {
         bool isDark = ThemeService.Instance.IsDarkTheme;
         double opacity = SettingsService.Instance.Current.OpacityPercent;
+        this.Opacity = opacity / 100.0;
         var accent = ThemeService.Instance.AccentColor;
 
         ApplyHardwareAcrylic();
